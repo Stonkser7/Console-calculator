864067f1a108d8552c2853499a4bf2bf3f860010
Stonkser <dorofeevvantey@yandex.ru>
2022-05-20 13:11:15 +0400

    added operations with variables

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,12 +1,21 @@
 /*
 Программа выполняет функции калькулятора
 
-Грамматика программы: 
+Грамматика программы:
+	
 
-Инструкция:
-	Выражение
+Вычисление:
+	Инструкция
 	Вывод
 	Выход
+	Вычисление Инструкция
+
+Инструкция:
+	Объявление
+	Выражение
+
+Объявление:
+	"let" Имя "=" Выражение
 
 Вывод:
 	;
@@ -29,7 +38,7 @@
 	Число
 	( Выражение )
 	-Первичное
-	+Первичное
+	Имя
 
 Число:
 	Литерал с плавающей точкой
@@ -43,12 +52,14 @@
 #include <iomanip>
 #include <ctime>
 #include <conio.h>
+#include <cctype>
 
 
-void error(std::string s)
+void error(std::string s1, std::string s2 = "")
 {
-	throw std::runtime_error(s);
+	throw std::runtime_error(s1 + s2);
 }
+
 
 
 const char print = ';';
@@ -57,17 +68,72 @@ const char number = '8';
 const std::string prompt = "> ";
 const std::string result = "= ";
 
+const char name = 'a';
+const char let = 'L';
+const std::string declkey = "let";
+
 class Token
 {
 public:
 	char kind;
 	double value;
+	std::string name;
+
+	Token() {};
+
+	Token(char ch) :kind{ ch } {};
+
+	Token(char ch, double d) :kind{ ch }, value{ d } {};
+
+	Token(char ch, std::string s) :kind{ ch }, name{ s } {};
 };
+
+class Variable {
+public:
+	std::string name;
+	double value;
+};
+
+std::vector <Variable> var_table;
+
+void set_value(std::string s, double d)
+	//присваивает объекту типа Variable с именем s значение d
+{
+	for (Variable v : var_table)
+		if (v.name == s)
+		{
+			v.value = d;
+			return;
+		}
+	error("set_value: неопределённая переменная");
+}
+double get_value(std::string s)
+	//возвращает значение переменной с именем s
+{
+	for (Variable v : var_table)
+		if (v.name == s) return v.value;
+	error("get_value: неопределённая переменная", s);
+}
+bool is_declared(std::string var)
+	//проверяет наличие переменной var в векторе var_table
+{
+	for (Variable v : var_table)
+		if (v.name == var) return true;
+	return false;
+}
+double define_name(std::string var, double val)
+	//добавляет пару (var, val) в вектор var_table
+{
+	if (is_declared(var)) error(var, " - неопределённая переменная");
+	var_table.push_back(Variable{ var, val });
+	return val;
+}
 
 class Token_stream {
 public:
 	Token get();
 	void putback(Token t);
+	void ignore(char c);	//отбрасывает символы до символа c включительно
 private:
 	bool full{ false };
 	Token buffer;
@@ -100,6 +166,7 @@ Token Token_stream::get()
 	case '*':
 	case '/':
 	case '%':
+	case '=':
 		return Token{ ch };
 	case '.':
 	case '0': case '1': case '2': case '3': case '4':
@@ -109,11 +176,45 @@ Token Token_stream::get()
 		std::cin >> val;
 		return Token{ number, val };
 	default:
-		error("invalid token");
+		if (std::isalpha(ch)) {
+			std::cin.putback(ch);
+			std::string s;
+			while (std::cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+			std::cin.putback(ch);
+			if (s == declkey)
+			{
+				return Token{ let };
+			}
+			return Token{ name, s };
+		}
+		error("undefined token");
 	}
 }
 
+void Token_stream::ignore(char c)
+{
+	if (full && c == buffer.kind) {	//проверяем буфер
+		full = false;
+		return;
+	}
+
+	full = false;
+
+	//проверяем входные данные
+	char ch = 0;
+	while (std::cin >> ch)
+		if (ch == c) return;
+}
+
+
+
 Token_stream ts;
+
+
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
 
 double expression();
 
@@ -136,8 +237,12 @@ double primary()
 		return t.value;
 	case '-':
 		return -primary();
-	case '+':
-		return primary();
+	case name:
+		if (is_declared(t.name)) {
+			double d = get_value(t.name);
+			return d;
+		}
+		error(t.name, " is not declared");
 	default:
 		error("required a number");
 	}
@@ -210,6 +315,32 @@ double expression()
 	}
 }
 
+double declaration()
+{
+	Token t = ts.get();
+	if (t.kind != name) error("В объявлении ожидается имя переменной");
+	std::string var_name = t.name;
+
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("Пропущен символ '=' в объявлении ", var_name);
+
+	double d = expression();
+	define_name(var_name, d);
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.putback(t);
+		return expression();
+	}
+}
+
 void start_message()
 {
 	std::cout << "Добро пожаловать в программу-калькулятор!" << std::endl
@@ -221,14 +352,20 @@ void start_message()
 void calculate()
 {
 	while (std::cin) {
-		std::cout << prompt;
-		Token t = ts.get();
-		while (t.kind == print)
-			t = ts.get();
-		if (t.kind == quit)
-			break;
-		ts.putback(t);
-		std::cout << result << expression() << std::endl;
+		try {
+			std::cout << prompt;
+			Token t = ts.get();
+			while (t.kind == print)
+				t = ts.get();
+			if (t.kind == quit)
+				break;
+			ts.putback(t);
+			std::cout << result << statement() << std::endl;
+		}
+		catch (std::exception& e) {
+			std::cerr << "ERROR : " << e.what() << std::endl;
+			clean_up_mess();
+		}
 	}
 }
 
@@ -237,11 +374,6 @@ int main()
 	srand(time(0));
 	rand();
 	SetConsoleOutputCP(1251);
-	try {
-		start_message();
-		calculate();
-	}
-	catch (std::exception& e) {
-		std::cerr << "ERROR : " << e.what() << std::endl;
-	}
+	start_message();
+	calculate();
 }
